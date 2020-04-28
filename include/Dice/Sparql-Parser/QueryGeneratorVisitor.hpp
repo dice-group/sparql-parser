7b54f3d645efd37e105090c0a078b2f73c8f6d84
shaheen <shaheen@mail.uni-paderborn.de>
2020-04-29 00:51:53 +0200

    implement term parsing in triple block

--- include/Dice/Sparql-Parser/QueryGeneratorVisitor.hpp
+++ include/Dice/Sparql-Parser/QueryGeneratorVisitor.hpp
@@ -228,18 +228,23 @@ public:
 
 
     antlrcpp::Any visitVarOrTerm(Dice::tentris::sparql::parser::SparqlParser::VarOrTermContext *ctx) override {
-        return SparqlParserBaseVisitor::visitVarOrTerm(ctx);
+        //visit term
+        if(ctx->graphTerm()!= nullptr)
+            return visitGraphTerm(ctx->graphTerm());
+        //visit var
+        else
+            return visitGraphTerm(ctx->graphTerm());
     }
 
 
-private:
+
     antlrcpp::Any visitGraphTerm(Dice::tentris::sparql::parser::SparqlParser::GraphTermContext *ctx) override {
         std::string termRaw=ctx->getText();
         rdf_parser::store::rdf::Term term=rdf_parser::Turtle::parseTerm(termRaw);
         return term;
     }
 
-public:
+
     antlrcpp::Any
     visitOptionalGraphPattern(Dice::tentris::sparql::parser::SparqlParser::OptionalGraphPatternContext *ctx) override {
         return SparqlBaseVisitor::visitOptionalGraphPattern(ctx);
