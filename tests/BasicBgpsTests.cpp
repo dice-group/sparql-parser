d1ae2c8f1c1d83e636c1f7dd96179049653a14b4
Fakhr shaheen <fakhrshaheen@gmail.com>
2020-12-18 19:10:25 +0100

    organize Test && fix some issues

--- tests/BasicBgpsTests.cpp
+++ tests/BasicBgpsTests.cpp
@@ -3,6 +3,138 @@
 
 using namespace SparqlQueryGraph::Nodes::SelectNodes;
 
+
+
+TEST(BasicBgpsTests, BasicSelectQueryDefaultTest) {
+    std::string query{
+            "SELECT ?book ?title ?price\n"
+            "WHERE\n"
+            "  { ?book <dc:title> ?title ;\n"
+            "         <ns:price> ?price ."
+            "  }"};
+
+    std::shared_ptr<SelectNode> selectNode=SparqlParser::Parser::parseSelectQuery(query);
+    auto operands=selectNode->getOperands();
+    auto bgps=selectNode->getBgps();
+    auto  prefixes=selectNode->getPrefixes();
+    auto subscriptResult=selectNode->getSubscriptResult();
+    auto selectVariables=selectNode->getSelectVariables();
+    auto selectModifier=selectNode->getSelectModifier();
+
+    std::vector<std::vector<char>> expectedOperands {{'a','b'},
+                                                     {'a','c'}
+                                                    };
+    std::vector<TriplePatternElement> expectedBgps{TriplePatternElement(TripleVariable("book"),Term("<dc:title>"),TripleVariable("title")),
+                                                   TriplePatternElement(TripleVariable("book"),Term("<ns:price>"),TripleVariable("price"))};
+
+    std::map<std::string,std::string> expectedPrefixes{};
+    std::vector<char> expectedSubscriptResult{'a','b','c'};
+    std::vector<TripleVariable> expectedSelectVariables{ TripleVariable{"book"},
+                                                         TripleVariable{"title"},
+                                                         TripleVariable{"price"}
+                                                       };
+
+    bool operandsEqual=std::equal(operands.begin(),operands.end(),expectedOperands.begin());
+    bool bgpsEqual=std::equal(bgps.begin(),bgps.end(),expectedBgps.begin());
+    bool prefixesEqual=std::equal(prefixes.begin(),prefixes.end(),expectedPrefixes.begin());
+    bool subscriptResultEqual=std::equal(subscriptResult.begin(),subscriptResult.end(),expectedSubscriptResult.begin());
+    bool selectVariablesEqual=std::equal(selectVariables.begin(),selectVariables.end(),expectedSelectVariables.begin());
+
+    ASSERT_EQ(operandsEqual,true);
+    ASSERT_EQ(bgpsEqual,true);
+    ASSERT_EQ(prefixesEqual,true);
+    ASSERT_EQ(subscriptResultEqual,true);
+    ASSERT_EQ(selectVariablesEqual,true);
+    ASSERT_EQ(selectModifier,SelectModifier::NONE);
+}
+
+TEST(BasicBgpsTests, BasicSelectQueryDistinctTest) {
+    std::string query{
+            "SELECT DISTINCT ?book ?title ?price\n"
+            "WHERE\n"
+            "  { ?book <dc:title> ?title ;\n"
+            "         <ns:price> ?price ."
+            "  }"};
+
+    std::shared_ptr<SelectNode> selectNode=SparqlParser::Parser::parseSelectQuery(query);
+    auto operands=selectNode->getOperands();
+    auto bgps=selectNode->getBgps();
+    auto  prefixes=selectNode->getPrefixes();
+    auto subscriptResult=selectNode->getSubscriptResult();
+    auto selectVariables=selectNode->getSelectVariables();
+    auto selectModifier=selectNode->getSelectModifier();
+
+    std::vector<std::vector<char>> expectedOperands {{'a','b'},
+                                                     {'a','c'}
+    };
+    std::vector<TriplePatternElement> expectedBgps{TriplePatternElement(TripleVariable("book"),Term("<dc:title>"),TripleVariable("title")),
+                                                   TriplePatternElement(TripleVariable("book"),Term("<ns:price>"),TripleVariable("price"))};
+
+    std::map<std::string,std::string> expectedPrefixes{};
+    std::vector<char> expectedSubscriptResult{'a','b','c'};
+    std::vector<TripleVariable> expectedSelectVariables{ TripleVariable{"book"},
+                                                         TripleVariable{"title"},
+                                                         TripleVariable{"price"}
+    };
+
+    bool operandsEqual=std::equal(operands.begin(),operands.end(),expectedOperands.begin());
+    bool bgpsEqual=std::equal(bgps.begin(),bgps.end(),expectedBgps.begin());
+    bool prefixesEqual=std::equal(prefixes.begin(),prefixes.end(),expectedPrefixes.begin());
+    bool subscriptResultEqual=std::equal(subscriptResult.begin(),subscriptResult.end(),expectedSubscriptResult.begin());
+    bool selectVariablesEqual=std::equal(selectVariables.begin(),selectVariables.end(),expectedSelectVariables.begin());
+
+    ASSERT_EQ(operandsEqual,true);
+    ASSERT_EQ(bgpsEqual,true);
+    ASSERT_EQ(prefixesEqual,true);
+    ASSERT_EQ(subscriptResultEqual,true);
+    ASSERT_EQ(selectVariablesEqual,true);
+    ASSERT_EQ(selectModifier,SelectModifier::DISTINCT);
+}
+
+TEST(BasicBgpsTests, BasicSelectQueryReducedTest) {
+    std::string query{
+            "SELECT REDUCED ?book ?title ?price\n"
+            "WHERE\n"
+            "  { ?book <dc:title> ?title ;\n"
+            "         <ns:price> ?price ."
+            "  }"};
+
+    std::shared_ptr<SelectNode> selectNode=SparqlParser::Parser::parseSelectQuery(query);
+    auto operands=selectNode->getOperands();
+    auto bgps=selectNode->getBgps();
+    auto  prefixes=selectNode->getPrefixes();
+    auto subscriptResult=selectNode->getSubscriptResult();
+    auto selectVariables=selectNode->getSelectVariables();
+    auto selectModifier=selectNode->getSelectModifier();
+
+    std::vector<std::vector<char>> expectedOperands {{'a','b'},
+                                                     {'a','c'}
+    };
+    std::vector<TriplePatternElement> expectedBgps{TriplePatternElement(TripleVariable("book"),Term("<dc:title>"),TripleVariable("title")),
+                                                   TriplePatternElement(TripleVariable("book"),Term("<ns:price>"),TripleVariable("price"))};
+
+    std::map<std::string,std::string> expectedPrefixes{};
+    std::vector<char> expectedSubscriptResult{'a','b','c'};
+    std::vector<TripleVariable> expectedSelectVariables{ TripleVariable{"book"},
+                                                         TripleVariable{"title"},
+                                                         TripleVariable{"price"}
+    };
+
+    bool operandsEqual=std::equal(operands.begin(),operands.end(),expectedOperands.begin());
+    bool bgpsEqual=std::equal(bgps.begin(),bgps.end(),expectedBgps.begin());
+    bool prefixesEqual=std::equal(prefixes.begin(),prefixes.end(),expectedPrefixes.begin());
+    bool subscriptResultEqual=std::equal(subscriptResult.begin(),subscriptResult.end(),expectedSubscriptResult.begin());
+    bool selectVariablesEqual=std::equal(selectVariables.begin(),selectVariables.end(),expectedSelectVariables.begin());
+
+    ASSERT_EQ(operandsEqual,true);
+    ASSERT_EQ(bgpsEqual,true);
+    ASSERT_EQ(prefixesEqual,true);
+    ASSERT_EQ(subscriptResultEqual,true);
+    ASSERT_EQ(selectVariablesEqual,true);
+    ASSERT_EQ(selectModifier,SelectModifier::REDUCE);
+}
+
+
 TEST(BasicBgpsTests, multipleBgps) {
     std::string query{
             "SELECT ?s ?e WHERE {?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://localhost/vocabulary/bench/Journal>.?s <http://swrc.ontoware.org/ontology#editor> ?e}"};
@@ -12,14 +144,35 @@ TEST(BasicBgpsTests, multipleBgps) {
     auto operands=selectNode->getOperands();
     auto bgps=selectNode->getBgps();
     auto  prefixes=selectNode->getPrefixes();
-    auto subscript=selectNode->getSubscriptResult();
+    auto subscriptResult=selectNode->getSubscriptResult();
     auto selectVariables=selectNode->getSelectVariables();
+    auto selectModifier=selectNode->getSelectModifier();
 
+    std::vector<std::vector<char>> expectedOperands {{'a'},
+                                                     {'a','b'}
+    };
+    std::vector<TriplePatternElement> expectedBgps{TriplePatternElement(TripleVariable("s"),Term("<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>"),Term("<http://localhost/vocabulary/bench/Journal>")),
+                                                   TriplePatternElement(TripleVariable("s"),Term("<http://swrc.ontoware.org/ontology#editor>"),TripleVariable("e"))};
 
-    ASSERT_EQ(true,true);
-    //ASSERT_EQ(iri,Term::make_term(iriString));
+    std::map<std::string,std::string> expectedPrefixes{};
+    std::vector<char> expectedSubscriptResult{'a','b'};
+    std::vector<TripleVariable> expectedSelectVariables{ TripleVariable{"s"},
+                                                         TripleVariable{"e"}
+                                                        };
+
+    bool operandsEqual=std::equal(operands.begin(),operands.end(),expectedOperands.begin());
+    bool bgpsEqual=std::equal(bgps.begin(),bgps.end(),expectedBgps.begin());
+    bool prefixesEqual=std::equal(prefixes.begin(),prefixes.end(),expectedPrefixes.begin());
+    bool subscriptResultEqual=std::equal(subscriptResult.begin(),subscriptResult.end(),expectedSubscriptResult.begin());
+    bool selectVariablesEqual=std::equal(selectVariables.begin(),selectVariables.end(),expectedSelectVariables.begin());
+
+    ASSERT_EQ(operandsEqual,true);
+    ASSERT_EQ(bgpsEqual,true);
+    ASSERT_EQ(prefixesEqual,true);
+    ASSERT_EQ(subscriptResultEqual,true);
+    ASSERT_EQ(selectVariablesEqual,true);
+    ASSERT_EQ(selectModifier,SelectModifier::NONE);
 }
-
 
 
 TEST(BasicBgpsTests, multipleBgps2) {
@@ -31,104 +184,34 @@ TEST(BasicBgpsTests, multipleBgps2) {
     auto operands=selectNode->getOperands();
     auto bgps=selectNode->getBgps();
     auto  prefixes=selectNode->getPrefixes();
-    auto subscript=selectNode->getSubscriptResult();
+    auto subscriptResult=selectNode->getSubscriptResult();
     auto selectVariables=selectNode->getSelectVariables();
+    auto selectModifier=selectNode->getSelectModifier();
 
+    std::vector<std::vector<char>> expectedOperands {{'a'},
+                                                     {'a','b','c'}
+    };
+    std::vector<TriplePatternElement> expectedBgps{TriplePatternElement(TripleVariable("s"),Term("<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>"),Term("<http://localhost/vocabulary/bench/Article>")),
+                                                   TriplePatternElement(TripleVariable("s"),TripleVariable("p"),TripleVariable("o"))};
 
-    ASSERT_EQ(true,true);
-    //ASSERT_EQ(iri,Term::make_term(iriString));
-}
+    std::map<std::string,std::string> expectedPrefixes{};
+    std::vector<char> expectedSubscriptResult{'a','b','c'};
+    std::vector<TripleVariable> expectedSelectVariables{ TripleVariable{"s"},
+                                                         TripleVariable{"p"},
+                                                         TripleVariable{"o"}
+    };
 
-TEST(BasicBgpsTests, BasicSelectQueryDefaultTest) {
-    std::string query{
-            "SELECT ?book ?nameY ?nickY\n"
-            "WHERE\n"
-            "  { ?book <dc:title> ?title ;\n"
-            "         <ns:price> ?price ."
-            "  }"};
+    bool operandsEqual=std::equal(operands.begin(),operands.end(),expectedOperands.begin());
+    bool bgpsEqual=std::equal(bgps.begin(),bgps.end(),expectedBgps.begin());
+    bool prefixesEqual=std::equal(prefixes.begin(),prefixes.end(),expectedPrefixes.begin());
+    bool subscriptResultEqual=std::equal(subscriptResult.begin(),subscriptResult.end(),expectedSubscriptResult.begin());
+    bool selectVariablesEqual=std::equal(selectVariables.begin(),selectVariables.end(),expectedSelectVariables.begin());
 
-    std::shared_ptr<SelectNode> selectNode=SparqlParser::Parser::parseSelectQuery(query);
-    auto operands=selectNode->getOperands();
-    auto bgps=selectNode->getBgps();
-    auto  prefixes=selectNode->getPrefixes();
-    auto subscript=selectNode->getSubscriptResult();
-    auto selectVariables=selectNode->getSelectVariables();
-
-    ASSERT_EQ(true,true);
-}
-
-TEST(BasicBgpsTests, BasicSelectQueryDistinctTest) {
-    std::string query{
-            "SELECT DISTINCT ?book ?nameY ?nickY\n"
-            "WHERE\n"
-            "  { ?book <dc:title> ?title ;\n"
-            "         <ns:price> ?price ."
-            "  }"};
-
-    std::shared_ptr<SelectNode> selectNode=SparqlParser::Parser::parseSelectQuery(query);
-    auto operands=selectNode->getOperands();
-    auto bgps=selectNode->getBgps();
-    auto  prefixes=selectNode->getPrefixes();
-    auto subscript=selectNode->getSubscriptResult();
-    auto selectVariables=selectNode->getSelectVariables();
-
-    ASSERT_EQ(true,true);
-}
-
-TEST(BasicBgpsTests, BasicSelectQueryReducedTest) {
-    std::string query{
-            "SELECT REDUCED ?book ?nameY ?nickY\n"
-            "WHERE\n"
-            "  { ?book <dc:title> ?title ;\n"
-            "         <ns:price> ?price ."
-            "  }"};
-
-    std::shared_ptr<SelectNode> selectNode=SparqlParser::Parser::parseSelectQuery(query);
-    auto operands=selectNode->getOperands();
-    auto bgps=selectNode->getBgps();
-    auto  prefixes=selectNode->getPrefixes();
-    auto subscript=selectNode->getSubscriptResult();
-    auto selectVariables=selectNode->getSelectVariables();
-
-    ASSERT_EQ(true,true);
-}
-
-TEST(BasicBgpsTests, BasicOptionalSelectQueryReducedTest) {
-    std::string query{
-            "SELECT REDUCED ?book ?nameY ?nickY\n"
-            "WHERE\n"
-            "  { ?book <dc:title> ?title ;\n"
-            "          <ns:price> ?price ."
-            "OPTIONAL { ?book <dc:author> <dc:title> }"
-            "  }"};
-
-    std::shared_ptr<SelectNode> selectNode=SparqlParser::Parser::parseSelectQuery(query);
-    auto operands=selectNode->getOperands();
-    auto bgps=selectNode->getBgps();
-    auto  prefixes=selectNode->getPrefixes();
-    auto subscript=selectNode->getSubscriptResult();
-    auto selectVariables=selectNode->getSelectVariables();
-
-    ASSERT_EQ(true,true);
-}
-
-TEST(BasicBgpsTests, ComplexOptionalSelectQueryReducedTest) {
-    std::string query{
-            "SELECT REDUCED ?book ?nameY ?nickY\n"
-            "WHERE\n"
-            "  { ?book <dc:title> ?title ;\n"
-            "          <ns:price> ?price ."
-            "OPTIONAL { ?book <dc:author> <dc:title> "
-            "  OPTIONAL { ?book <dc:isbn> ?isbn. } \n } \n"
-            "  }"};
-
-    std::shared_ptr<SelectNode> selectNode=SparqlParser::Parser::parseSelectQuery(query);
-    auto operands=selectNode->getOperands();
-    auto bgps=selectNode->getBgps();
-    auto  prefixes=selectNode->getPrefixes();
-    auto subscript=selectNode->getSubscriptResult();
-    auto selectVariables=selectNode->getSelectVariables();
-
-    ASSERT_EQ(true,true);
+    ASSERT_EQ(operandsEqual,true);
+    ASSERT_EQ(bgpsEqual,true);
+    ASSERT_EQ(prefixesEqual,true);
+    ASSERT_EQ(subscriptResultEqual,true);
+    ASSERT_EQ(selectVariablesEqual,true);
+    ASSERT_EQ(selectModifier,SelectModifier::DISTINCT);
 
 }
--- tests/BasicOptionalPatternTests.cpp
+++ tests/BasicOptionalPatternTests.cpp
@@ -3,6 +3,9 @@
 
 using namespace SparqlQueryGraph::Nodes::SelectNodes;
 
+
+
+
 TEST(BasicOptionalPatternTests, multipleBgpsBeforeOptional) {
     std::string query{
             "SELECT ?s ?t ?e WHERE {?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://localhost/vocabulary/bench/Journal> . ?s <http://purl.org/dc/elements/1.1/title> ?t . OPTIONAL{?s <http://swrc.ontoware.org/ontology#editor> ?e}}"};
@@ -12,12 +15,40 @@ TEST(BasicOptionalPatternTests, multipleBgpsBeforeOptional) {
     auto operands=selectNode->getOperands();
     auto bgps=selectNode->getBgps();
     auto  prefixes=selectNode->getPrefixes();
-    auto subscript=selectNode->getSubscriptResult();
+    auto subscriptResult=selectNode->getSubscriptResult();
     auto selectVariables=selectNode->getSelectVariables();
+    auto selectModifier=selectNode->getSelectModifier();
 
+    std::vector<std::vector<char>> expectedOperands {{'a'},
+                                                     {'a','b'},
+                                                     {'['},
+                                                     {'a','c'},
+                                                     {']'}
+    };
+    std::vector<TriplePatternElement> expectedBgps{TriplePatternElement(TripleVariable("s"),Term("<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>"),Term("<http://localhost/vocabulary/bench/Journal>")),
+                                                   TriplePatternElement(TripleVariable("s"),Term("<http://purl.org/dc/elements/1.1/title>"),TripleVariable("t")),
+                                                   TriplePatternElement(TripleVariable("s"),Term("<http://swrc.ontoware.org/ontology#editor>"),TripleVariable("e"))
+                                                   };
 
-    ASSERT_EQ(true,true);
-    //ASSERT_EQ(iri,Term::make_term(iriString));
+    std::map<std::string,std::string> expectedPrefixes{};
+    std::vector<char> expectedSubscriptResult{'a','b','c'};
+    std::vector<TripleVariable> expectedSelectVariables{ TripleVariable{"s"},
+                                                         TripleVariable{"t"},
+                                                         TripleVariable{"e"}
+    };
+
+    bool operandsEqual=std::equal(operands.begin(),operands.end(),expectedOperands.begin());
+    bool bgpsEqual=std::equal(bgps.begin(),bgps.end(),expectedBgps.begin());
+    bool prefixesEqual=std::equal(prefixes.begin(),prefixes.end(),expectedPrefixes.begin());
+    bool subscriptResultEqual=std::equal(subscriptResult.begin(),subscriptResult.end(),expectedSubscriptResult.begin());
+    bool selectVariablesEqual=std::equal(selectVariables.begin(),selectVariables.end(),expectedSelectVariables.begin());
+
+    ASSERT_EQ(operandsEqual,true);
+    ASSERT_EQ(bgpsEqual,true);
+    ASSERT_EQ(prefixesEqual,true);
+    ASSERT_EQ(subscriptResultEqual,true);
+    ASSERT_EQ(selectVariablesEqual,true);
+    ASSERT_EQ(selectModifier,SelectModifier::NONE);
 }
 
 TEST(BasicOptionalPatternTests, multipleBgpsInsideOptional) {
@@ -29,10 +60,39 @@ TEST(BasicOptionalPatternTests, multipleBgpsInsideOptional) {
     auto operands=selectNode->getOperands();
     auto bgps=selectNode->getBgps();
     auto  prefixes=selectNode->getPrefixes();
-    auto subscript=selectNode->getSubscriptResult();
+    auto subscriptResult=selectNode->getSubscriptResult();
     auto selectVariables=selectNode->getSelectVariables();
+    auto selectModifier=selectNode->getSelectModifier();
 
+    std::vector<std::vector<char>> expectedOperands {{'a','b'},
+                                                     {'['},
+                                                     {'a','c'},
+                                                     {'c','d'},
+                                                     {']'}
+    };
+    std::vector<TriplePatternElement> expectedBgps{TriplePatternElement(TripleVariable("a"),Term("<http://purl.org/dc/elements/1.1/title>"),TripleVariable("t")),
+                                                   TriplePatternElement(TripleVariable("a"),Term("<http://swrc.ontoware.org/ontology#journal>"),TripleVariable("j")),
+                                                   TriplePatternElement(TripleVariable("j"),Term("<http://swrc.ontoware.org/ontology#editor>"),TripleVariable("e"))
+    };
 
-    ASSERT_EQ(true,true);
-    //ASSERT_EQ(iri,Term::make_term(iriString));
+    std::map<std::string,std::string> expectedPrefixes{};
+    std::vector<char> expectedSubscriptResult{'a','b','c','d'};
+    std::vector<TripleVariable> expectedSelectVariables{ TripleVariable{"a"},
+                                                         TripleVariable{"t"},
+                                                         TripleVariable{"j"},
+                                                         TripleVariable{"e"}
+    };
+
+    bool operandsEqual=std::equal(operands.begin(),operands.end(),expectedOperands.begin());
+    bool bgpsEqual=std::equal(bgps.begin(),bgps.end(),expectedBgps.begin());
+    bool prefixesEqual=std::equal(prefixes.begin(),prefixes.end(),expectedPrefixes.begin());
+    bool subscriptResultEqual=std::equal(subscriptResult.begin(),subscriptResult.end(),expectedSubscriptResult.begin());
+    bool selectVariablesEqual=std::equal(selectVariables.begin(),selectVariables.end(),expectedSelectVariables.begin());
+
+    ASSERT_EQ(operandsEqual,true);
+    ASSERT_EQ(bgpsEqual,true);
+    ASSERT_EQ(prefixesEqual,true);
+    ASSERT_EQ(subscriptResultEqual,true);
+    ASSERT_EQ(selectVariablesEqual,true);
+    ASSERT_EQ(selectModifier,SelectModifier::NONE);
 }
