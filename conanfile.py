e596b349f8395ae7a3e564f52f8f37e3862faab9
Fakhr shaheen <fakhrshaheen@gmail.com>
2021-01-19 11:28:07 +0100

    delelte unused files

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,6 @@ from conans import ConanFile, CMake,tools
 
 class sparqlParserBase(ConanFile):
     name = "sparql-parser"
-    version = "0.1.0"
     author = "DICE Group <info@dice-research.org>"
     description = "SPARQL parser."
     homepage = "https://github.com/dice-group/sparql-parser"
@@ -12,6 +11,8 @@ class sparqlParserBase(ConanFile):
     topics = "SPARQL", "parser", "semantic web"
     settings = "os", "compiler", "build_type", "arch"
     requires="sparql-parser-base/0.2.2@dice-group/stable","sparql-query/0.10@dice-group/stable","rdf-parser/0.12@dice-group/stable","robin-hood-hashing/3.9.1","gtest/1.8.1"
+    options = {'with_tests': [False, True]}
+    default_options = {'with_tests': False}
     generators = "cmake", "cmake_find_package", "cmake_paths"
     exports = "LICENSE.txt"
     exports_sources = (
@@ -20,9 +21,14 @@ class sparqlParserBase(ConanFile):
          "include/*")
     no_copy_source = True
 
+    def requirements(self):
+        if self.options.with_tests:
+            self.requires("gtest/1.8.1")
+
     def package(self):
         cmake = CMake(self)
-        cmake.definitions["rdf_parser_BUILD_TESTS"] = "OFF"
+        if self.options.with_tests:
+            cmake.definitions["SPARQL_PARSER_BUILD_TESTS"] = "ON"
         cmake.configure()
         cmake.install()
 
